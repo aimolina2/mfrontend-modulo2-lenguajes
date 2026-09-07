6974cf0258f0c89b65de5162c1d60d2a8492e507
Ainoa <aimmolina2@gmail.com>
2026-09-07 21:04:04 +0200

    initial commit

---
+++ src/02-entregables/ejercicio-01.ts
@@ -0,0 +1,8 @@
+console.log("************** DELIVERABLE 01 *********************");
+
+// 1. HEAD
+// Implementa una función head (inmutable), tal que, dado un
+// array como entrada extraiga y devuelva su primer elemento.
+// Utiliza destructuring.
+
+const head = (/* array */) => {}; // Implementation here.
