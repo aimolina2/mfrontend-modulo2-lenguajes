fe4e4d7eb5c3c86da8a074708e39fb5b9566f3a3
Ainoa <aimmolina2@gmail.com>
2026-09-07 21:25:07 +0200

    exercise 1 started

--- src/02-entregables/ejercicio-01.ts
+++ src/02-entregables/ejercicio-01.ts
@@ -5,4 +5,22 @@ console.log("************** DELIVERABLE 01 *********************");
 // array como entrada extraiga y devuelva su primer elemento.
 // Utiliza destructuring.
 
-const head = (/* array */) => {}; // Implementation here.
+const colors: string[] = ["red", "blue", "green", "yellow", "orange"];
+
+const head = ([first]: string[]) => first; // Implementation here.
+console.log("HEAD: ", head(colors));
+
+// 2. TAIL
+// Implementa una función tail (inmutable),
+// tal que, dado un array como entrada devuelta todos menos
+// el primer elemento. Utiliza rest operator.
+
+const tail = ([, ...rest]: string[]) => rest; // Implementation here.
+console.log("TAIL: ", tail(colors));
+
+console.log("ORIGINAL ARRAY: ", colors);
+
+// 3. INIT
+// Implementa una función init (inmutable), tal que,
+// dado un array como entrada devuelva todos los elementos
+// menos el último. Utiliza los métodos que ofrece Array.prototype.
