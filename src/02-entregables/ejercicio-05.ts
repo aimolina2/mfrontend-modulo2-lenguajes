623454dbfc8fe6fb2030c86c1ee84f4ee79b716f
Ainoa <aimmolina2@gmail.com>
2026-09-19 12:25:54 +0200

    exercise 5 completed

--- src/02-entregables/ejercicio-05.ts
+++ src/02-entregables/ejercicio-05.ts
@@ -1,1 +1,50 @@
 console.log("************** DELIVERABLE 05 *********************");
+
+// 1. SLOT MACHINE
+
+const getRandomBoolean = (): boolean => Math.random() >= 0.5;
+
+class SlotMachine {
+  coins: number;
+
+  constructor() {
+    this.coins = 0;
+  }
+
+  play(): void {
+    this.coins++;
+    const result = [getRandomBoolean(), getRandomBoolean(), getRandomBoolean()];
+    if (result[0] && result[1] && result[2]) {
+      console.log(`Congratulations!!!. You won ${this.coins} coins!!`);
+      this.coins = 0;
+    } else {
+      console.log("Good luck next time!!");
+    }
+  }
+}
+
+const machine1 = new SlotMachine();
+console.log("******** MACHINE 1 ********");
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+
+const machine2 = new SlotMachine();
+console.log("******** MACHINE 2 ********");
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
